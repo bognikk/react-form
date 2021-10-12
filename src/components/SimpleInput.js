cd63f3654b7c457a10f9fc30ce6c761bb6407931
Nikola Bogicevic <nbogicevic2@gmail.com>
2021-10-12 13:35:12 -0400

    Getting user input values with OnChange and Ref

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmisionHandler = (event) => {
+    event.preventDefault();
+
+    console.log("STATE", enteredName);
+    const enteredValue = nameInputRef;
+    console.log("REF", enteredValue.current);
+
+    // nameInputRef.current.value = ""; => NOT IDEAL, DON'T MANIPULATE THE DOM
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmisionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          ref={nameInputRef}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
